ec3a212b5affafcaa3fd8960e9e26b69a22d30ca
Somye <mahajan.somye@gmail.com>
2024-12-30 14:11:54 +0530

    Added success feedback in db io

--- lib/db/database_crud.dart
+++ lib/db/database_crud.dart
@@ -1,66 +1,63 @@
 import 'package:dotenv/dotenv.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 import 'package:prompt_chat/cli/server.dart';
 import 'package:prompt_chat/db/connect.dart';
 import 'package:prompt_chat/cli/user.dart';
 
 class DatabaseIO {
   DatabaseIO();
-  static Future<void> addToDB(dynamic document, String collectionName) async {
-    var env = DotEnv(includePlatformEnvironment: true)
-    ..load(['.env']);
-    var db = await connectDB(
-        env['MONGO_URI']!);
+  static Future<bool> addToDB(dynamic document, String collectionName) async {
+    var env = DotEnv(includePlatformEnvironment: true)..load(['.env']);
+    var db = await connectDB(env['MONGO_URI']!);
     //db logic
     var reqCollection = db.collection(collectionName);
-    await reqCollection.insertOne(document.toMap());
+    WriteResult result = await reqCollection.insertOne(document.toMap());
     db.close();
+    return result.isSuccess;
   }
 }
 
 class UserIO extends DatabaseIO {
   UserIO._();
   static Future<List<User>> getAllUsers() async {
-    var env = DotEnv(includePlatformEnvironment: true)
-    ..load(['.env']);
-    var db = await connectDB(
-        env['MONGO_URI']!);
+    var env = DotEnv(includePlatformEnvironment: true)..load(['.env']);
+    var db = await connectDB(env['MONGO_URI']!);
     var reqUsers =
         await db.collection("users").find({"finder": "finder"}).toList();
     db.close();
     return reqUsers.map((e) => User.fromMap(e)).toList();
   }
-  static Future<void> updateDB(dynamic document) async {
-    var env = DotEnv(includePlatformEnvironment: true)
-    ..load(['.env']);
-    var db = await connectDB(
-        env['MONGO_URI']!);
+
+  static Future<bool> updateDB(dynamic document) async {
+    var env = DotEnv(includePlatformEnvironment: true)..load(['.env']);
+    var db = await connectDB(env['MONGO_URI']!);
     var reqCollection = db.collection("users");
-    await reqCollection.replaceOne({'username': document.username}, document.toMap());
+    WriteResult writeResult = await reqCollection
+        .replaceOne({'username': document.username}, document.toMap());
     db.close();
+    return writeResult.isSuccess;
   }
 }
 
 class ServerIO extends DatabaseIO {
   ServerIO._();
   static Future<List<Server>> getAllServers() async {
-    var env = DotEnv(includePlatformEnvironment: true)
-    ..load(['.env']);
-    var db = await connectDB(
-        env['MONGO_URI']!);
+    var env = DotEnv(includePlatformEnvironment: true)..load(['.env']);
+    var db = await connectDB(env['MONGO_URI']!);
     var reqServers =
         await db.collection("servers").find({"finder": "finder"}).toList();
     db.close();
-    if(reqServers == [null]) {
-    }
+    if (reqServers == [null]) {}
     return reqServers.map((e) => Server.fromMap(e)).toList();
   }
-  static Future<void> updateDB(dynamic document) async {
-    var env = DotEnv(includePlatformEnvironment: true)
-    ..load(['.env']);
-    var db = await connectDB(
-        env['MONGO_URI']!);
+
+  static Future<bool> updateDB(dynamic document) async {
+    var env = DotEnv(includePlatformEnvironment: true)..load(['.env']);
+    var db = await connectDB(env['MONGO_URI']!);
     var reqCollection = db.collection("servers");
-    await reqCollection.replaceOne({'serverName': document.serverName}, document.toMap());
+    WriteResult writeResult = await reqCollection
+        .replaceOne({'serverName': document.serverName}, document.toMap());
     db.close();
+    return writeResult.isSuccess;
   }
 }
