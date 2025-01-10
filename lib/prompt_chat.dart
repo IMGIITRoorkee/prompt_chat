a89b80abdbb6d56d3803e2ebef3aac6cc5d5a3c0
Somye <mahajan.somye@gmail.com>
2025-01-10 23:35:25 +0530

    Fixed

--- lib/prompt_chat.dart
+++ lib/prompt_chat.dart
@@ -1,5 +1,4 @@
 import 'dart:core';
-import 'dart:io';
 import 'package:fuzzywuzzy/fuzzywuzzy.dart';
 import 'package:intl/intl.dart' as intl;
 import 'package:prompt_chat/cli/category.dart';
@@ -40,12 +39,12 @@ class ChatAPI {
   // Register a user
   Future<void> registerUser(String? username, String? password) async {
     if (username == null || password == null) {
-      
       throw InvalidCredentialsException();
     }
     if (!isPasswordValid(password)) {
-     print("Password must be atleast 8 characters long, having atleast a number & a special character. Please try again.");
-    throw WeakPasswordException();
+      print(
+          "Password must be atleast 8 characters long, having atleast a number & a special character. Please try again.");
+      throw WeakPasswordException();
     }
     validUsername(username);
     var newUser = User(username, password, false);
@@ -220,6 +219,9 @@ class ChatAPI {
     if (serverName == null || userName == null) {
       throw Exception(
           "Please enter the required credentials, or login to continue.");
+    }
+    if (serverName.isEmpty) {
+      throw Exception("Server name cannot be empty");
     }
     JoinPerm joinPerm = getJoinPerm(serverPerm);
     var creator = getUser(userName);
@@ -567,20 +569,18 @@ class ChatAPI {
     var reqServer = getServer(servername);
     var reqUser = getUser(username);
     reqServer.checkAccessLevels(username, [1, 2]);
-    var inviteCode = InviteCode(reqUser, "",reqServer);
-    for(var invitecode in reqServer.inviteCodes ){
-      if(invitecode.code == inviteCode.code){
+    var inviteCode = InviteCode(reqUser, "", reqServer);
+    for (var invitecode in reqServer.inviteCodes) {
+      if (invitecode.code == inviteCode.code) {
         return createInviteCode(servername, username);
-      }
-      else if (invitecode.inviter == reqUser){
+      } else if (invitecode.inviter == reqUser) {
         return invitecode.code;
       }
     }
     reqServer.inviteCodes.add(inviteCode);
-    await DatabaseIO.addToDB(inviteCode,"invitecodes");
+    await DatabaseIO.addToDB(inviteCode, "invitecodes");
     inviteCodes.add(inviteCode);
     return inviteCode.code;
-
   }
 
   // join server using invite code
@@ -596,7 +596,7 @@ class ChatAPI {
     invite.invitedUsers.add(reqUser);
   }
 
-  void sendDm(String recieverusername, String message, String senderusername){
+  void sendDm(String recieverusername, String message, String senderusername) {
     User sender = getUser(senderusername);
     User reciever = getUser(recieverusername);
     DirectMessage dm = DirectMessage(sender, reciever, message);
@@ -604,24 +604,24 @@ class ChatAPI {
     dm.send();
   }
 
-  Future<List<String>> getRecievedDms(String username) async{
+  Future<List<String>> getRecievedDms(String username) async {
     User user = getUser(username);
     List<DirectMessage> dms = await DirectMessage.getMessages(user);
     List<String> messages = [];
-    for(DirectMessage dm in dms){
-      if(dm.receiver.username == user.username){
+    for (DirectMessage dm in dms) {
+      if (dm.receiver.username == user.username) {
         messages.add("${dm.sender.username} : ${dm.message}");
       }
     }
     return messages;
   }
 
-  Future<List<String>> getSentDms(String username) async{
+  Future<List<String>> getSentDms(String username) async {
     User user = getUser(username);
     List<DirectMessage> dms = await DirectMessage.getMessages(user);
     List<String> messages = [];
-    for(DirectMessage dm in dms){
-      if(dm.sender.username == user.username){
+    for (DirectMessage dm in dms) {
+      if (dm.sender.username == user.username) {
         messages.add("${dm.receiver.username} : ${dm.message}");
       }
     }
